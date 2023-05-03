0c57778bd8fdb0ddd0384fa4f0764d83858977c4
arya <2020csb1107@iitrpr.ac.in>
2023-05-03 15:22:59 +0530

    mid

--- server/middleware.js
+++ server/middleware.js
@@ -1,36 +1,33 @@
-var {getAuth} = require("firebase-admin/auth");
+const { getAuth } = require("firebase-admin/auth");
 
 async function authorizeUser(req, res, next) {
-    // get token from header
-    // let idToken = req.header("Authorization");
-    //
-    //
-    // if (!idToken) {
-    //   return res.status(401).json({
-    //     message: "Access Denied",
-    //     error: "No token provided",
-    //   });
-    // } else {
-    //   idToken = idToken.split(" ")[1];
-    //   console.log(idToken)
-    //   getAuth()
-    //     .verifyIdToken(idToken)
-    //     .then((decodedToken) => {
-    //       const uid = decodedToken.uid;
-    //       req.user_id = uid;
-    //       next();
-    //     })
-    //     .catch((error) => {
-    //       console.log(error);
-    //       return res.status(401).json({
-    //         message: "Access Denied",
-    //         error: "Invalid token",
-    //       });
-    //     });
-    // }
-  }
+  // Get token from header
+  let idToken = req.header("Authorization");
 
+  if (!idToken) {
+    return res.status(401).json({
+      message: "Access Denied",
+      error: "No token provided",
+    });
+  } else {
+    idToken = idToken.split(" ")[1];
+    console.log(idToken);
+
+    try {
+      const decodedToken = await getAuth().verifyIdToken(idToken);
+      const uid = decodedToken.uid;
+      req.user_id = uid;
+      next();
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json({
+        message: "Access Denied",
+        error: "Invalid token",
+      });
+    }
+  }
+}
 
 module.exports = {
-    authorizeUser
-}
+  authorizeUser,
+};
